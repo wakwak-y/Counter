fb620585a35f31f7aa3c28bc5c3836df7f4d3264
Yu Wakui <wak121.y@gmail.com>
2024-09-10 06:23:30 +0900

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Yu Wakui on 2024/09/10.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
