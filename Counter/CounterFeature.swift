31a17293f6d3f4ee1c0196705aa5ae927d74c779
Yu Wakui <wak121.y@gmail.com>
2024-09-11 07:55:11 +0900

    Adding side effects

--- Counter/CounterFeature.swift
+++ Counter/CounterFeature.swift
@@ -5,6 +5,7 @@
 //  Created by Yu Wakui on 2024/09/10.
 //
 
+import Foundation
 import ComposableArchitecture
 
 @Reducer
@@ -14,13 +15,24 @@ struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading = false
+        var isTimerRunning = false
     }
     
     // ユーザーが実行できるアクションを定義
     enum Action {
         // ユーザーがUIで実際に行う操作に基づいて定義する
         case decrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
         case incrementButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
+    }
+    
+    enum CancelID {
+        case timer
     }
     
     var body: some ReducerOf<Self> {
@@ -29,10 +41,49 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
+                
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                
+                return .run { [count = state.count ] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+                
+            case .factResponse(let fact):
+                state.fact = fact
+                state.isLoading = false
+                return .none
+                
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
+            
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+                
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
             }
         }
     }
--- Counter/CounterView.swift
+++ Counter/CounterView.swift
@@ -38,6 +38,31 @@ struct CounterView: View {
                     .background(Color.black.opacity(0.1))
                     .cornerRadius(10)
                 }
+                
+                Button(store.isTimerRunning ? "Stop Timer" : "Start timer") {
+                    store.send(.toggleTimerButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                Button("Fact") {
+                    store.send(.factButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                if store.isLoading {
+                    ProgressView()
+                } else if let fact = store.fact {
+                    Text(fact)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                }
             }
         }
     }
